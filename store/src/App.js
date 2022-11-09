1a8389e0ac07bfb69cdd885ecf5a34eaf355eacd
Saman Mahalati <mahalatisaman@gmail.com>
2022-11-09 18:04:21 +0330

    hello

---
+++ store/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//Components
+import Navbar from './components/Navbar';
+
+import "./App.css" 
+
+
+const App = () => {
+  return (
+    <div className='App'>
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
