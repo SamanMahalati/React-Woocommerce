b130749d04ec1716fabb113d80b3231176e536f3
Saman Mahalati <mahalatisaman@gmail.com>
2022-11-11 18:30:06 +0330

    Create function for sum price and sum quantity

--- store/src/context/CartContextProvider.js
+++ store/src/context/CartContextProvider.js
@@ -8,6 +8,12 @@ const initialState = {
     checkout: false,
 }
 
+const sumItems = item => {
+    const itemsCounter = item.reduce((total , product) => total + product.quantity , 0 )
+    const total = item.reduce((total , product) => total + product.price * product.quantity , 0 ).toFixed(2)
+    return {total , itemsCounter}
+}
+
 const cartReducer = (state, action) => {
     switch (action.type) {
         case "ADD_ITEM":
