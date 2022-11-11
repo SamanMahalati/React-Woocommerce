71c2988dfcb74492374f2dd3aafc1ae1f7b42834
Saman Mahalati <mahalatisaman@gmail.com>
2022-11-11 17:51:22 +0330

    Create function for get selected product quantity

--- store/src/helper/functions.js
+++ store/src/helper/functions.js
@@ -3,4 +3,13 @@ const isInCart = (state , id) => {
     return result
 }
 
-export {isInCart}
+const quantityCount = (state , id) => {
+    const index = state.selectedItems.findIndex(item => item.id === id)
+    if (index === -1) {
+        return false
+    } else {
+        return state.selectedItems[index].quantity
+    }
+}
+
+export {isInCart , quantityCount }
