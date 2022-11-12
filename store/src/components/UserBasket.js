48c3bbfb824b206b1804c3b156242aecab1f0eb0
Saman Mahalati <mahalatisaman@gmail.com>
2022-11-12 18:01:25 +0330

    Create userBasket empty cart with style

--- store/src/components/UserBasket.js
+++ store/src/components/UserBasket.js
@@ -1,5 +1,8 @@
 import React, { useContext } from 'react';
 
+//React Router Dom
+import { Link } from 'react-router-dom'
+
 //Components
 import UserBasketCart from './UserBasketCart';
 
@@ -9,12 +12,27 @@ import { CartContext } from '../context/CartContextProvider';
 //Style
 import style from "./UserBasket.module.css"
 
+//images
+import empty from "../images/empty-cart.svg"
+
 const UserBasket = () => {
     const { state } = useContext(CartContext)
+
     return (
         <section className={style.UserBasketContainer}>
             {
-                state.selectedItems.map(item => <UserBasketCart key={item.id} data={item}/>)
+                state.itemsCounter ?
+                    state.selectedItems.map(item => <UserBasketCart key={item.id} data={item} />) :
+                    <div className={style.emptyContainer}>
+                        <img src={empty} alt="empty" />
+                        <h2> سبد خرید شما خالی است ! </h2>
+                        <h5>می‌توانید برای مشاهده محصولات بیشتر به صفحات زیر بروید:</h5>
+                        <div className={style.emptyLinkContainer}>
+                            <Link className={style.emptyLink} to="/">صفحه اصلی</Link>
+                            |
+                            <Link className={style.emptyLink} to="/products">صفحه محصولات</Link>
+                        </div>
+                    </div>
             }
         </section>
     );
